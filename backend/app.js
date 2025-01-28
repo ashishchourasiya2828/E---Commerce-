435e73c72d37124632dd60f28a6c2f73e7545e46
Ashish Chourasiya <ashishchourasiya2828@gmai.com>
2025-01-28 22:39:30 +0530

    routing added by abhishek

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+
+const userRoutes = require("./src/routes/user.routes")
+
+app.use('/users',userRoutes)
+
+
+
+
+
+module.exports = app
